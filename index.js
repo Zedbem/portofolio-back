ee048c55de95fb0ea53f814374354e2e9fb3a30c
Zedbem <98301704+Zedbem@users.noreply.github.com>
2024-01-30 23:33:04 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const cors = require('cors');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+
+const port = 3000;
+const app = express();
+app.use(cors());
+
+const projects = [{
+    name: "Telegram-бот Кобаяши",
+    description: "Бот-органайзер с возможностью подсчета дневных КБЖУ",
+    image: "static/kobayashi.jpg"
+}, {
+    name: "Война Вирусов",
+    description: "Стратегическая игра, в которой игроки сражаются за контроль над виртуальным миром при помощи вирусов",
+    image: "static/viruswar.jpg"
+}]
+
+app.get('/projects', (req, res) => {
+    res.send(projects);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(port, ()=>{
+    console.log(`App is listening http://localhost:${port}/`);
+})
